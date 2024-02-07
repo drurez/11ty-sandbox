78cf6391461383828540f8bdd15c46ca7b045844
Andrew Juarez <andrewjuarez@hotmail.com>
2024-02-06 23:48:26 -0800

    more sandboxing

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,17 @@
 	module.exports = function(eleventyConfig) {
 
 	// Aliases are in relation to the _includes folder
-		eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
+	eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
+		
+	// ignore source assets (processing and watching)
+	eleventyConfig.ignores.add("./src/assets/**/*");
+  eleventyConfig.watchIgnores.add("./src/assets/**/*");
 
-		eleventyConfig.addPassthroughCopy("./src/assets/style.css");
+  // server config (watch generated assets in _site folder)
+  eleventyConfig.setServerOptions({
+    watch: ["./_site/assets/css/**/*.css"],
+    port: 3000,
+  });
 
 	return {
 		dir: { 
